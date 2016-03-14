7cb8cf52ae285308ab095a6812566dd60635f25b
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-14 07:57:29 +0100

    Add initial bootstrap script
    Note that the script name in the package.json file is wrong. The
    postinstall step must be tested. An infinite loop is possible.

---
+++ bootstrap.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const fs     = require ('fs');
+const path   = require ('path');
+const co     = require ('co');
+const spawn  = require ('co-child-process');
+const rimraf = require ('rimraf');
+
+
+function* git () {
+  const args = Array.prototype.slice.call (arguments);
+  console.log (`git ${args.join (' ')}`);
+
+  yield spawn ('git', args, {
+    stdio: ['ignore', 1, 2],
+    cwd: __dirname
+  });
+}
+
+function* npm (verb, modPath) {
+  console.log (`npm ${verb} ${modPath}`);
+
+  let args = [verb];
+  if (Array.isArray (modPath)) {
+    args = args.concat (modPath);
+  } else {
+    args.push (modPath);
+  }
+
+  yield spawn ('npm', args, {
+    stdio: ['ignore', 1, 2],
+    cwd: __dirname
+  });
+}
+
+function* rm (nodeModPath) {
+  console.log (`rm ${nodeModPath}`);
+
+  yield rimraf.sync (path.join (__dirname, nodeModPath)) || {};
+}
+
+function parsePackage (packagePath) {
+  let def = fs.readFileSync (path.join (__dirname, packagePath));
+  def = JSON.parse (def);
+
+  let list = {};
+
+  [def.dependencies, def.devDependencies]
+    .forEach (deps => {
+      Object
+        .keys (deps)
+        .filter (pkg => {
+          return !/^electrum(?:-arc)?$/.test (pkg);
+        })
+        .forEach (pkg => {
+          list[`${pkg}@${deps[pkg]}`] = null;
+        });
+    });
+
+  return Object.keys (list);
+}
+
+co (function* () {
+  yield* git ('submodule', 'update', '--init', '--recursive');
+  yield* git ('submodule', 'foreach', '--recursive', 'git checkout master');
+  yield* git ('submodule', 'foreach', '--recursive', 'git pull');
+
+  let list = [];
+  list = list.concat (
+    parsePackage ('electrum-starter-3/package.json'),
+    parsePackage ('electrum/package.json'),
+    parsePackage ('electrum-arc/package.json')
+  );
+  yield* npm ('install', list);
+
+  fs.symlinkSync ('../electrum',     './node_modules/electrum');
+  fs.symlinkSync ('../electrum-arc', './node_modules/electrum-arc');
+}).then (() => {
+  console.log ('done');
+}, err => {
+  console.error (err.stack);
+});
